5b4bd99cd2c5897e46d455972ab161b7b1d2b232
useEffects <joel.sr1024@gmail.com>
2023-11-10 15:49:44 +0530

    formatting

--- second-round/set-1/2.cpp
+++ second-round/set-1/2.cpp
@@ -4,38 +4,38 @@
 using namespace std;
 
 bool isPerfectSquare(int num) {
-  int sqrtNum = static_cast<int>(sqrt(num));
-  return (sqrtNum * sqrtNum == num);
+    int sqrtNum = static_cast<int>(sqrt(num));
+    return (sqrtNum * sqrtNum == num);
 }
 
 bool isMultipleOf4(int num) {
-  int divNum = static_cast<int>(num / 4);
-  return divNum * 4 == num;
+    int divNum = static_cast<int>(num / 4);
+    return divNum * 4 == num;
 }
 
 bool isDivisibleby6(int num) {
-  int mulNum = static_cast<int>(num / 6);
-  return mulNum * 6 == num;
+    int mulNum = static_cast<int>(num / 6);
+    return mulNum * 6 == num;
 }
 
 int main() {
-  vector<int> myVector = {10, 36, 54, 89, 12};
-  vector<int> ans(myVector.size());
-  for (int i = 0; i < myVector.size(); i++) {
-    int weight = 0;
-    if (isPerfectSquare(myVector[i])) {
-      weight += 5;
+    vector<int> myVector = {10, 36, 54, 89, 12};
+    vector<int> ans(myVector.size());
+    for (int i = 0; i < myVector.size(); i++) {
+        int weight = 0;
+        if (isPerfectSquare(myVector[i])) {
+            weight += 5;
+        }
+        if (isMultipleOf4(myVector[i]) && isDivisibleby6(myVector[i])) {
+            weight += 4;
+        }
+        if (myVector[i] % 2 == 0) {
+            weight += 3;
+        }
+        ans[i] = weight;
     }
-    if (isMultipleOf4(myVector[i]) && isDivisibleby6(myVector[i])) {
-      weight += 4;
+    for (int i = 0; i < myVector.size(); i++) {
+        cout << myVector[i] << "\t" << ans[i] << "\n";
     }
-    if (myVector[i] % 2 == 0) {
-      weight += 3;
-    }
-    ans[i] = weight;
-  }
-  for (int i = 0; i < myVector.size(); i++) {
-    cout << myVector[i] << "\t" << ans[i] << "\n";
-  }
-  return 0;
+    return 0;
 }
